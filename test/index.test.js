e7c9acce959b43dcf4018d53ba129c8ee4f7501c
J.D <jd@phon.one>
2023-10-14 11:27:43 +0300

    Add tests

---
+++ test/index.test.js
@@ -0,0 +1,79 @@
+const assert = require('assert')
+const lib = require('../index')
+const secret = 'secret'
+const data = { foo: 'bar' }
+const token = lib.sign(data, secret)
+const [head, body] = token.split('.')
+const decode = x => JSON.parse(atob(x))
+
+describe('Library supports:', () => {
+  const hs256 = lib.sign(data, secret, 'HS256')
+  const hs384 = lib.sign(data, secret, 'HS384')
+  const hs512 = lib.sign(data, secret, 'HS512')
+
+  const head = token => token.split('.')[0]
+
+  it('HS256: HMAC using SHA-256 hash', () =>
+    assert.equal(decode(head(hs256)).alg, 'HS256'))
+
+  it('HS384: HMAC using SHA-384 hash', () =>
+    assert.equal(decode(head(hs384)).alg, 'HS384'))
+
+  it('HS512: HMAC using SHA-512 hash', () =>
+    assert.equal(decode(head(hs512)).alg, 'HS512'))
+})
+
+describe('Library is a object which exports:', () => {
+  assert.equal(typeof lib, 'object')
+
+  describe(`'sign' method which:`, () => {
+    assert.equal(typeof lib.sign, 'function')
+
+    describe(`return token from ${JSON.stringify(data)}:`, () => {
+      it(token, () =>
+        assert.ok(token, 'iat is `null` or `undefined`'))
+    })
+
+    describe(`adds defaults to head (${head}):`, () => {
+      const { typ, alg } = decode(head)
+
+      it(`'typ: JWT'`, () =>
+        assert.equal(typ, 'JWT'))
+
+      it(`'alg: HS256'`, () =>
+        assert.equal(alg, 'HS256'))
+    })
+
+    describe(`adds iat prop:`, () => {
+      const { iat } = decode(body)
+
+      it(`to body (${body})`, () =>
+        assert.ok(iat, 'iat is `null` or `undefined`'))
+    })
+  })
+
+  describe(`'verify' method which:`, () => {
+    assert.equal(typeof lib.verify, 'function')
+
+    describe(`'verify' return data from token:`, () => {
+      const { iat } = decode(body)
+
+      it(JSON.stringify({ ...data, iat }), () =>
+        assert.deepEqual({ ...data, iat }, lib.verify(token, secret)))
+    })
+
+    describe(`'verify' return errors:`, () => {
+      const invalid = () => lib.verify(token, 'secre')
+      const expired = lib.sign({ exp: Date.now() }, secret)
+      const exp = () => lib.verify(expired, secret)
+
+      it('for invalid signature', () =>
+        assert.throws(invalid, new Error('Invalid signature')))
+
+
+      it('for expired token', () =>
+        assert.throws(exp, new Error('Token expired')))
+
+    })
+  })
+})
